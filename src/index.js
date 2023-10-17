17309670753a80d568915fbcce96cc34dce104b9
Jack Witt <Jwitt1452@gmail.com>
2023-10-17 15:00:22 -0700

    catch errors

--- src/index.js
+++ src/index.js
@@ -1,46 +1,50 @@
 #! /usr/bin/env node
 import { input, select } from "@inquirer/prompts";
-import chalk from "chalk";
-import gradient from "gradient-string";
 
 import { parse } from "./helpers/fs.js";
 import { formatChoices } from "./helpers/format.js";
-import { greet, passion } from "./helpers/log.js";
+import { info, greet, passion } from "./helpers/log.js";
 import { greetingEnabled, getNameFromFlag } from "./helpers/flags.js";
 
 import { cook } from "./cook.js";
 
-const cookbook = parse();
+try {
+  const cookbook = parse();
 
-if (greetingEnabled()) greet();
+  if (greetingEnabled()) greet();
 
-let name = getNameFromFlag();
-if (!name) {
-  name = await input({
-    message: "What would you like to name your project?",
-    validate: (str) => str.length > 0,
+  let name = getNameFromFlag();
+  if (!name) {
+    name = await input({
+      message: "What would you like to name your project?",
+      validate: (str) => str.length > 0,
+    });
+  }
+  const recipe = { name };
+  console.log(`Cooking up some ${passion(name)}\n`);
+
+  const selectedTemplate = await select({
+    message: "What template would you like to use?",
+    choices: formatChoices(Object.keys(cookbook)),
   });
+  recipe.template = selectedTemplate;
+
+  const prompts = cookbook[selectedTemplate].map(
+    ({ key, message, options }) => ({
+      key,
+      message,
+      choices: formatChoices(options),
+    })
+  );
+
+  for (const { key, message, choices } of prompts) {
+    await select({
+      message,
+      choices,
+    }).then((selection) => (recipe[key] = selection));
+  }
+
+  cook(recipe);
+} catch (e) {
+  info("\n" + passion("bye!"));
 }
-const recipe = { name };
-console.log(`Cooking up some ${passion(name)}\n`);
-
-const selectedTemplate = await select({
-  message: "What template would you like to use?",
-  choices: formatChoices(Object.keys(cookbook)),
-});
-recipe.template = selectedTemplate;
-
-const prompts = cookbook[selectedTemplate].map(({ key, message, options }) => ({
-  key,
-  message,
-  choices: formatChoices(options),
-}));
-
-for (const { key, message, choices } of prompts) {
-  await select({
-    message,
-    choices,
-  }).then((selection) => (recipe[key] = selection));
-}
-
-cook(recipe);
